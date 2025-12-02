e985ea2ed709487a9d90115811367bbae495fdbc
albertprofe <albertprofe@gmail.com>
2025-12-02 13:32:47 +0100

    feat: define Apartment class and CommandLineRunner

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/Apartment.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/Apartment.java
@@ -3,5 +3,185 @@ package com.example.apartment_predictor;
 public class Apartment {
 
     private String id;
+    private Long price;
+    private Integer area;
+    private Integer bedrooms;
+    private Integer bathrooms;
+    private Integer stories;
+    private String mainroad;
+    private String guestroom;
+    private String basement;
+    private String hotwater;
+    private String heating;
+    private String airconditioning;
+    private Integer parking;
+    private String prefarea;
+    private String furnishingstatus;
 
+    // Default constructor
+    public Apartment() {}
+
+    // Constructor with all fields
+    public Apartment(Long price, Integer area, Integer bedrooms, Integer bathrooms, 
+                    Integer stories, String mainroad, String guestroom, String basement,
+                    String hotwater, String heating, String airconditioning, 
+                    Integer parking, String prefarea, String furnishingstatus) {
+        this.price = price;
+        this.area = area;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.stories = stories;
+        this.mainroad = mainroad;
+        this.guestroom = guestroom;
+        this.basement = basement;
+        this.hotwater = hotwater;
+        this.heating = heating;
+        this.airconditioning = airconditioning;
+        this.parking = parking;
+        this.prefarea = prefarea;
+        this.furnishingstatus = furnishingstatus;
+    }
+
+
+
+    // Getters and Setters
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public Integer getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(Integer bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public Integer getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(Integer bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public Integer getStories() {
+        return stories;
+    }
+
+    public void setStories(Integer stories) {
+        this.stories = stories;
+    }
+
+    public String getMainroad() {
+        return mainroad;
+    }
+
+    public void setMainroad(String mainroad) {
+        this.mainroad = mainroad;
+    }
+
+    public String getGuestroom() {
+        return guestroom;
+    }
+
+    public void setGuestroom(String guestroom) {
+        this.guestroom = guestroom;
+    }
+
+    public String getBasement() {
+        return basement;
+    }
+
+    public void setBasement(String basement) {
+        this.basement = basement;
+    }
+
+    public String getHotwater() {
+        return hotwater;
+    }
+
+    public void setHotwater(String hotwater) {
+        this.hotwater = hotwater;
+    }
+
+    public String getHeating() {
+        return heating;
+    }
+
+    public void setHeating(String heating) {
+        this.heating = heating;
+    }
+
+    public String getAirconditioning() {
+        return airconditioning;
+    }
+
+    public void setAirconditioning(String airconditioning) {
+        this.airconditioning = airconditioning;
+    }
+
+    public Integer getParking() {
+        return parking;
+    }
+
+    public void setParking(Integer parking) {
+        this.parking = parking;
+    }
+
+    public String getPrefarea() {
+        return prefarea;
+    }
+
+    public void setPrefarea(String prefarea) {
+        this.prefarea = prefarea;
+    }
+
+    public String getFurnishingstatus() {
+        return furnishingstatus;
+    }
+
+    public void setFurnishingstatus(String furnishingstatus) {
+        this.furnishingstatus = furnishingstatus;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    /*public void setId(String id) {
+        this.id = id;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "price=" + price +
+                ", area=" + area +
+                ", bedrooms=" + bedrooms +
+                ", bathrooms=" + bathrooms +
+                ", stories=" + stories +
+                ", mainroad='" + mainroad + '\'' +
+                ", guestroom='" + guestroom + '\'' +
+                ", basement='" + basement + '\'' +
+                ", hotwater='" + hotwater + '\'' +
+                ", heating='" + heating + '\'' +
+                ", airconditioning='" + airconditioning + '\'' +
+                ", parking=" + parking +
+                ", prefarea='" + prefarea + '\'' +
+                ", furnishingstatus='" + furnishingstatus + '\'' +
+                '}';
+    }
 }
--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/ApartmentPredictorApplication.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/ApartmentPredictorApplication.java
@@ -1,16 +1,104 @@
 package com.example.apartment_predictor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class ApartmentPredictorApplication {
+public class ApartmentPredictorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApartmentPredictorApplication.class, args);
+	}
 
-        System.out.println("Hello java class");
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Creating apartment objects...");
+		
+		// Create apartment objects based on your sample data
+		Apartment apartment1 = new Apartment(
+				13300000L,    // price
+				7420,         // area
+				4,            // bedrooms
+				2,            // bathrooms
+				3,            // stories
+				"yes",        // mainroad
+				"no",         // guestroom
+				"no",         // basement
+				"no",         // hotwater
+				"yes",        // heating
+				"yes",        // airconditioning
+				2,            // parking
+				"yes",        // prefarea
+				"furnished"   // furnishingstatus
+		);
 
+
+		
+		// Create additional sample apartments
+		Apartment apartment2 = new Apartment(
+				8500000L,     // price
+				5200,         // area
+				3,            // bedrooms
+				2,            // bathrooms
+				2,            // stories
+				"yes",        // mainroad
+				"yes",        // guestroom
+				"no",         // basement
+				"yes",        // hotwater
+				"no",         // heating
+				"yes",        // airconditioning
+				1,            // parking
+				"no",         // prefarea
+				"semi-furnished" // furnishingstatus
+		);
+		
+		Apartment apartment3 = new Apartment(
+				6200000L,     // price
+				3800,         // area
+				2,            // bedrooms
+				1,            // bathrooms
+				1,            // stories
+				"no",         // mainroad
+				"no",         // guestroom
+				"yes",        // basement
+				"yes",        // hotwater
+				"no",         // heating
+				"no",         // airconditioning
+				0,            // parking
+				"yes",        // prefarea
+				"unfurnished" // furnishingstatus
+		);
+		
+		// Display the created apartments
+		System.out.println("\n=== Created Apartments ===");
+		System.out.println("Apartment 1: " + apartment1);
+		System.out.println("\nApartment 2: " + apartment2);
+		System.out.println("\nApartment 3: " + apartment3);
+		
+		System.out.println("\n=== Apartment Details ===");
+		printApartmentDetails(apartment1, "Luxury Apartment");
+		printApartmentDetails(apartment2, "Family Apartment");
+		printApartmentDetails(apartment3, "Budget Apartment");
+	}
+	
+	private void printApartmentDetails(Apartment apartment, String description) {
+		System.out.println("\n" + description + ":");
+		System.out.println("  Price: $" + apartment.getPrice());
+		System.out.println("  Area: " + apartment.getArea() + " sq ft");
+		System.out.println("  Bedrooms: " + apartment.getBedrooms());
+		System.out.println("  Bathrooms: " + apartment.getBathrooms());
+		System.out.println("  Stories: " + apartment.getStories());
+		System.out.println("  Main Road Access: " + apartment.getMainroad());
+		System.out.println("  Guest Room: " + apartment.getGuestroom());
+		System.out.println("  Basement: " + apartment.getBasement());
+		System.out.println("  Hot Water: " + apartment.getHotwater());
+		System.out.println("  Heating: " + apartment.getHeating());
+		System.out.println("  Air Conditioning: " + apartment.getAirconditioning());
+		System.out.println("  Parking Spaces: " + apartment.getParking());
+		System.out.println("  Preferred Area: " + apartment.getPrefarea());
+		System.out.println("  Furnishing Status: " + apartment.getFurnishingstatus());
 	}
 
 }
